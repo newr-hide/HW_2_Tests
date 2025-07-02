f0596341d8853677902b441586cefa6ff9206151
Bondarev Artem <bond210484@mail.ru>
2025-07-02 13:26:21 +0700

    add testEnvironment: jsdom,

--- jest.config.js
+++ jest.config.js
@@ -7,7 +7,8 @@
 //   }
 
 module.exports = {
-  testEnvironment: 'node', // Меняем jsdom на node
+  // testEnvironment: 'node',
+  testEnvironment: "jsdom",
   moduleNameMapper: {
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/__mocks__/fileMock.js',
   },
